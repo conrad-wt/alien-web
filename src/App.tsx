cc83f350b18c95c846043b8603a2b31b6b251dab
conrad-wt <conrad.wt97@hotmail.com>
2024-10-13 20:47:40 +0200

    feat: create an application able to communicate with backend server

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Spinner } from 'react-bootstrap';
+import BasicExample from './navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import useGetAliens from './fetchAliens';
+import { Alien } from './Alien';
 
 function App() {
+
+  const getAliens = useGetAliens()
+
+  if (getAliens.isLoading) {
+    return <Spinner/>
+  }
+
+  if (getAliens.error) {
+    return <div>
+      {getAliens.error.name} - {getAliens.error.message} - {getAliens.error.stack}
+    </div>
+  }
+
+  const mapAlien = (alien: Alien) => {
+    return <div>
+      {alien.id} - {alien.name} - {alien.tech}
+    </div>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BasicExample/>
+      <h1>Aliens</h1>
+      {getAliens.data?.map((alien) => mapAlien(alien))}
     </div>
   );
 }
